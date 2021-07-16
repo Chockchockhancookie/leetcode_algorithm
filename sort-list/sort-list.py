b76fd5fbc9ea6546bca52447eefd2f6752e05743
Chockchockhancookie <39588891+Chockchockhancookie@users.noreply.github.com>
2021-07-16 13:39:31 +0900

    Time: 152 ms (98.37%), Space: 30.3 MB (38.72%) - LeetHub

---
+++ sort-list/sort-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        p = head
+        arr = []
+        while p:
+            arr.append(p.val)
+            p = p.next
+            
+        arr.sort()
+        p = head
+        for i in range(len(arr)):
+            p.val = arr[i]
+            p = p.next
+            
+        return head
